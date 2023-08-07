2fb4626a0afdf87ec9b4131c940701ce5133e2a8
Shivpreet <shivpreet16@gmail.com>
2023-08-07 11:59:34 -0700

    added responsive navbar, changes in main hero area

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,13 +4,13 @@ import Image from "next/image";
 
 const Navbar = () => {
   return (
-    <div className="max-w-screen flex bg-[#a8a8a8] h-fit justify-between mt-10 mx-5 px-10 py-4 items-center dark:bg-light/10 rounded-full">
+    <div className="max-w-screen hidden md:flex bg-[#a8a8a8] h-fit justify-between my-10 mx-5 md:px-6 lg:px-10 py-4 items-center dark:bg-light/10 rounded-full ">
       <div className="flex gap-4 items-center">
         <Image src="./logo-white.svg" alt="" className="filter invert dark:filter-none w-10 h-10" width={15} height={15} />
-        <h1 className="uppercase text-5xl tracking-[14px] cursor-pointer">Ibento</h1>
+        <h1 className="uppercase md:text-3xl lg:text-5xl tracking-[14px] cursor-pointer">Ibento</h1>
       </div>
       <div>
-        <ul className="flex items-center gap-20 text-lg">
+        <ul className="flex items-center md:gap-10 lg:gap-20  lg:text-xl">
           <li className="cursor-pointer">Home</li>
           <li className="cursor-pointer">About</li>
           <li className="cursor-pointer">Login</li>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,6 +17,26 @@ export default function Home() {
         className={`bg-light dark:bg-dark min-h-screen h-fit text-dark dark:text-light overflow-x-hidden transition duration-100 ease-in-out`}
       >
         <Navbar />
+        <section className="flex w-full mx-16 h-[70vh]">
+          <div className="flex flex-col w-[60vw] gap-10">
+            <div className=" border-dark dark:border-light border-2 gap-8 h-full flex flex-col items-center justify-center">
+              <h1 className="uppercase text-7xl">Ibento</h1>
+              <div className="flex flex-col gap-4">
+                <h3 className="text-lg">Work Smart, Event Hard</h3>
+                <button className="text-dark bg-light px-9 py-2">
+                  Get Started
+                </button>
+              </div>
+            </div>
+            <button className="uppercase h-32 text-dark bg-light p-4">
+              <div className="w-full h-full border-4 text-2xl tracking-[18px] border-dark grid items-center">
+                Get in touch
+              </div>
+            </button>
+          </div>
+
+          <div></div>
+        </section>
       </main>
     </div>
   );
