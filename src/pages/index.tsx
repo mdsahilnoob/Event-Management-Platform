e09800db518e786f958c01d3412c70393a6ec31d
Jayanti2919 <jayanti2919@gmail.com>
2023-08-08 07:02:41 +0530

    changed mobile view

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,12 +19,12 @@ export default function Home() {
       <main
         className={`bg-light dark:bg-dark min-h-screen h-fit text-dark dark:text-light overflow-x-hidden transition duration-100 ease-in-out`}
       >
-        <section className="flex flex-col w-full mx-16 h-screen mb-10">
+        <section className="flex flex-col w-full md:mx-16 px-2 h-screen mb-10 md:mt-0 mt-10">
           <Navbar />
-          <div className="flex gap-24">
-            <div className="flex flex-col w-[60vw] gap-10">
+          <div className="flex gap-24 md:flex-row flex-col">
+            <div className="flex flex-col md:w-[60vw] w-full gap-10">
               <div className=" border-dark dark:border-light border-2 gap-8 h-full flex flex-col items-center justify-center">
-                <h1 className="uppercase text-7xl tracking-[24px]">Ibento</h1>
+                <h1 className="uppercase text-7xl md:tracking-[24px] tracking-widest">Ibento</h1>
                 <div className="flex flex-col gap-4">
                   <h3 className="text-lg">Work Smart, Event Hard</h3>
                   <button className="text-dark bg-light px-9 py-2">
@@ -33,7 +33,7 @@ export default function Home() {
                 </div>
               </div>
               <button className="uppercase h-32 text-dark bg-light_tert dark:bg-light p-4">
-                <div className="w-full h-full border-4 text-2xl tracking-[18px] border-dark flex justify-center items-center hover:gap-2 transition ease-in duration-100">
+                <div className="w-full h-full border-4 text-2xl md:tracking-[18px] tracking-wide border-dark flex justify-center items-center hover:gap-2 transition ease-in duration-100">
                   Get in touch
                   <AiOutlineArrowRight />
                 </div>
