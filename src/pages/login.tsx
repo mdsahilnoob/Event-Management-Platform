c0eb13d810c812fe1f91d697c5b39da2e02388cd
Jayanti2919 <jayanti2919@gmail.com>
2023-08-08 23:30:18 +0530

    started login

---
+++ src/pages/login.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import Navbar from '@/components/Navbar'
+import { Input, Button } from '@material-tailwind/react'
+
+const Login = () => {
+    const handleFormSubmit = () => {
+        console.log("Submitting")
+    }
+  return (
+    <div className='bg-light dark:bg-dark px-5 md:px-0 min-h-screen h-fit text-dark dark:text-light overflow-x-hidden transition duration-100 ease-in-out'>
+        <div className='fixed top-0 z-10 flex md:w-full justify-center'>
+        <Navbar />
+        </div>
+        <div className='flex mt-44'>
+            <form action="" onSubmit={handleFormSubmit}>
+                <Input
+                    type='text'
+                    name='Email'
+                    size='lg'
+                    placeholder='Email'
+                    required
+                    className='px-5 py-2 rounded-md border-light_tert dark:border-light outline-none border-2 bg-light dark:bg-dark'
+                />
+                <Input
+                    type='password'
+                    name='Password'
+                    size='lg'
+                    placeholder='Password'
+                    required
+                    className='px-5 py-2 rounded-md border-light_tert dark:border-light outline-none border-2 bg-light dark:bg-dark'
+                />
+                <Button className='bg-light_tert w-full py-2 text-md font-medium tracking' type='submit'>
+                    Submit
+                </Button>
+            </form>
+        </div>
+    </div>
+  )
+}
+
+export default Login
