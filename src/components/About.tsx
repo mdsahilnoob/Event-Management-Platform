2aa237c523912902d7d382a347a01beb60669c0b
Jayanti2919 <jayanti2919@gmail.com>
2023-08-10 20:38:04 +0530

    added about with responsive

---
+++ src/components/About.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Image from 'next/image'
+
+const About = () => {
+  return (
+    <div className='flex md:flex-row flex-col md:gap-0 gap-5 w-full justify-between pb-10 md:px-16 px-2 min-h-[50vh]'>
+        <div className='dark:bg-light bg-dark md:w-1/4 flex md:flex-col w-full flex-row items-center justify-between mr-10'>
+            <div className='w-full h-8 dark:bg-light bg-dark'></div>
+            <div className='w-full h-8 bg-light dark:bg-dark'></div>
+            <Image src='/japanese-ibento.svg' alt='japanese-ibento' height={32} width={32} className='filter invert dark:filter-none h-1/2 w-full hidden md:block'/>
+            <Image src='/japanese-ibento-horizontal.svg' alt='japanese-ibento' height={32} width={32} className='filter invert dark:filter-none h-full w-full md:hidden block'/>
+            <div className='w-full h-8 bg-light dark:bg-dark'></div>
+            <div className='w-full h-8 dark:bg-light bg-dark'></div>
+        </div>
+        <div className='border-dark dark:border-light border-2 md:px-20 px-10 py-10 w-full flex flex-col justify-center'>
+            <h2 className='text-4xl tracking-[8px] leading-relaxed font-medium max-w-sm'>IBENTO CAN CHANGE THE WAY YOU PLAN EVENTS</h2>
+            <p className='text-xl mt-5'>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Culpa ullam explicabo architecto magni quod harum molestias vitae animi aut ipsum necessitatibus eius ex quasi, veritatis porro debitis minima voluptas reiciendis.
+            </p>
+        </div>
+    </div>
+  )
+}
+
+export default About
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,8 @@ import { AiOutlineArrowRight } from "react-icons/ai";
 import NewsCards from "@/components/NewsCards";
 import { heroNews } from "../../constants";
 import { useState } from "react";
+import About from "@/components/About";
+import { Button } from "@material-tailwind/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,8 +23,8 @@ export default function Home() {
         <div className="fixed top-0 z-10 flex md:w-full justify-center">
           <Navbar active={active} setActive={setActive} />
         </div>
-        <div className="px-5 md:px-0">
-          <section className="flex flex-col w-full md:mx-16 px-2 mb-10 h-fit md:mt-44 mt-16 ">
+        <div className="px-5 md:px-2">
+          <section className="flex pb-10 flex-col w-full md:mx-16 mb-10 h-fit md:mt-44 mt-16 ">
             <div className="flex gap-24 md:flex-row flex-col">
               <div className="flex flex-col md:w-[60vw] w-full gap-10">
                 <div className={`border-dark dark:border-light border-2 gap-8 p-10 md:p-0 h-full flex flex-col items-center justify-center ${inter.className} light dark`}>
@@ -31,17 +33,17 @@ export default function Home() {
                   </h1>
                   <div className="flex flex-col gap-4">
                     <h3 className="text-lg">Work Smart, Event Hard</h3>
-                    <button className={`text-dark bg-light dark:bg-light_tert px-9 py-2 light dark`}>
+                    <Button className={`text-dark bg-light dark:bg-light_tert font-regular px-9 py-2 light dark`}>
                       Get Started
-                    </button>
+                    </Button>
                   </div>
                 </div>
-                <button className={`uppercase h-20 md:h-32 text-dark bg-light_tert dark:bg-light p-4 light dark`}>
-                  <div className="w-full h-full border-4 md:text-2xl text-md md:tracking-[18px] tracking-wide border-dark flex justify-center items-center hover:gap-2 transition ease-in duration-100">
+                <Button className={`uppercase h-20 md:h-32 text-dark bg-light_tert font-normal dark:bg-light p-4 light dark`}>
+                  <div className="w-full h-full border-2 md:text-2xl text-md md:tracking-[18px] tracking-wide border-dark flex justify-center items-center hover:gap-2 transition ease-in duration-100">
                     Get in touch
                     <AiOutlineArrowRight />
                   </div>
-                </button>
+                </Button>
               </div>
 
               <div className="flex flex-col gap-5 w-1/2">
@@ -59,8 +61,8 @@ export default function Home() {
               </div>
             </div>
           </section>
-          <section>
-            
+          <section className="mt-10">
+            <About />
           </section>
         </div>
       </main>
