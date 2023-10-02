dbae506427aa7d2d7c2d1b71a1b417bf5577bda7
Samrat1507 <lazyworm3325@gmail.com>
2023-10-02 23:44:20 +0530

    added dashboard nav

---
+++ src/components/Dashnav.tsx
@@ -0,0 +1,87 @@
+import React, { useState, useEffect } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { useSnapshot } from "valtio";
+import state from "../../store";
+import { Sling as Hamburger } from "hamburger-react";
+import Switcher from "./Switcher"; 
+
+const LaptopNav = () => {
+  const snap = useSnapshot(state);
+
+  return (
+    <div className="w-full bg-dark/20 backdrop-blur-md h-16 flex justify-between items-center py-4 px-6 md:px-10">
+      <div className="flex items-center">
+        <Image
+          src="./logo-white.svg"
+          alt=""
+          className="filter invert dark:filter-none w-10 h-10"
+          width={15}
+          height={15}
+        />
+        <h1 className="uppercase text-3xl lg:text-5xl tracking-[14px] cursor-pointer">
+          <Link href="/">Ibento</Link>
+        </h1>
+      </div>
+      <div>
+        <ul className="flex gap-10 lg:gap-16 lg:text-xl">
+          <li
+            className={`cursor-pointer ${
+              snap.active === "dashboard"
+                ? "text-light font-semibold dark:text-tertiary"
+                : ""
+            }`}
+            onClick={(e) => {
+              e.preventDefault();
+              state.active = "dashboard";
+            }}
+          >
+            <Link href="/dashboard">Dashboard</Link>
+          </li>
+          <li
+            className={`cursor-pointer ${
+              snap.active === "createEvent"
+                ? "text-light font-semibold dark:text-tertiary"
+                : ""
+            }`}
+            onClick={(e) => {
+              e.preventDefault();
+              state.active = "createEvent";
+            }}
+          >
+            <Link href="/create-event">Create Event</Link>
+          </li>
+          <li
+            className={`cursor-pointer ${
+              snap.active === "allEvents"
+                ? "text-light font-semibold dark:text-tertiary"
+                : ""
+            }`}
+            onClick={(e) => {
+              e.preventDefault();
+              state.active = "allEvents";
+            }}
+          >
+            <Link href="/all-events">All Events</Link>
+          </li>
+          <li
+            className={`cursor-pointer ${
+              snap.active === "trackEvents"
+                ? "text-light font-semibold dark:text-tertiary"
+                : ""
+            }`}
+            onClick={(e) => {
+              e.preventDefault();
+              state.active = "trackEvents";
+            }}
+          >
+            <Link href="/track-events">Track your events</Link>
+          </li>
+          <Switcher />
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default LaptopNav;
