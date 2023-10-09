1a4212fe3c6a6036412af2951de0fc0c1286dbeb
Jayanti2919 <jayanti2919@gmail.com>
2023-10-10 00:25:44 +0530

    added requirements frontend

---
+++ src/components/InputArea.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const InputArea = () => {
+  return (
+    <div>
+        <input type="text" className="py-2 px-2 bg-light border-2 border-dark" />
+    </div>
+  )
+}
+
+export default InputArea
